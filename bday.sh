407f5ed90bdf7e54fa9d52559ca6d17a24a7cfd2
Juan Pasamontes Fernández <103833998+JPdev417@users.noreply.github.com>
2024-03-31 23:15:16 +0200

    Update bday.sh

--- bday.sh
+++ bday.sh
@@ -21,6 +21,23 @@ else
 fi
 # use this ^ to check if file is downloaded or not, and open in TextEdit
 
-curl https://birthday-seven-sand.vercel.app/bday.wav -s  > /tmp/song.ogg && afplay /tmp/song.wav # Download and play the song
+# URL of the audio or video file you want to download
+file_url="https://supersecretive.vercel.app/bday.mp3"
 
-rm /tmp/song.wav # Remove the song after playing
+# Destination path where the file will be saved
+destination="/tmp/song.mp3"
+
+# Download the file using curl
+curl -sSL "$file_url" -o "$destination"
+
+# Check if the file was successfully downloaded
+if [ -f "$destination" ]; then
+    # Open the file using QuickTime Player
+    open -a "QuickTime Player" "$destination"
+    osascript -e 'tell application "QuickTime Player" to activate' -e 'tell application "System Events" to keystroke " "'
+else
+    echo "Failed to download the file."
+fi
+
+sleep 60 # Wait for 60 seconds, then remove the file
+rm /tmp/song.mp3 # Remove the song after playing
